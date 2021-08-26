74b63209229bd9b5b46501021c5b448e3b2cdbe5
Antonio Magana <antonioemagana@gmail.com>
2021-08-25 19:05:47 -0700

    components added

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div>
+            <h1>This is the footer</h1>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/TopNav.js
@@ -0,0 +1,9 @@
+export const TopNav = () => {
+    return (
+        <div class='topnav'>
+            <h1>This is the nav bar</h1>     
+        </div>
+    )
+}
+
+export default TopNav
