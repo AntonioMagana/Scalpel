988a9e887191bcab34642ba58fe137e75042596d
sair <sair_abbas@yahoo.com>
2021-11-09 19:44:50 -0800

    update config

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,13 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDyhfv3g5djAjAbLm4rDBhaGhAw7MkG05M",
-  authDomain: "scalpel-sad.firebaseapp.com",
-  projectId: "scalpel-sad",
-  storageBucket: "scalpel-sad.appspot.com",
-  messagingSenderId: "829852408226",
-  appId: "1:829852408226:web:62c534603777208964fafb",
-  measurementId: "G-86D8NPMYW5"
+  apiKey: "AIzaSyA7_U_fPfy231e1qUfF8rs1fmYg8QgtRLI",
+  authDomain: "scalpel-sad-835fc.firebaseapp.com",
+  projectId: "scalpel-sad-835fc",
+  storageBucket: "scalpel-sad-835fc.appspot.com",
+  messagingSenderId: "789366459633",
+  appId: "1:789366459633:web:d7e56dde7732024770ad50"
 };
 
 firebase.initializeApp(firebaseConfig);
